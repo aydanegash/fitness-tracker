e77fbc7484c4cc987b69fec9f0677f536ab7898d
aydanegash <73369360+aydanegash@users.noreply.github.com>
2021-08-16 17:33:24 -0500

    Create server.js
    created server.js file and got app to run

---
+++ server.js
@@ -0,0 +1,31 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const router = require("express").Router();
+const path = require("path");
+
+const PORT = process.env.PORT || 3000;
+
+const app = express();
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.use(express.static("public"));
+
+app.use(require("./routes/api.js"));
+app.use(require('./routes/view.js'));
+
+
+module.exports = router;
+
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/fitness-tracker", {
+  useNewUrlParser: true,
+  useFindAndModify: false
+});
+
+
+app.listen(PORT, () => {
+  console.log(`App running on port ${PORT}!`);
+});
+
+
