1dbb5e1dc986c5082cc7897db6ce05c875e44d01
aydanegash <73369360+aydanegash@users.noreply.github.com>
2021-08-17 03:30:16 -0500

    Update server.js
    added get routes for views

--- server.js
+++ server.js
@@ -13,8 +13,14 @@ app.use(express.json());
 app.use(express.static("public"));
 
 app.use(require("./routes/api.js"));
-app.use(require('./routes/view.js'));
 
+router.get("/exercise", (req, res) => {
+  res.sendFile(path.join(__dirname, "../public/exercise.html"));
+});
+
+router.get("/stats", (req, res) => {
+  res.sendFile(path.join(__dirname, "../public/stats.html"));
+});
 
 module.exports = router;
 
